92b297bbdf2a34a55eb2958afded2dc3dc20a3e4
KrGil <kanjjang89@naver.com>
2021-03-10 09:52:02 +0900

    def_class  add

---
+++ HELLOPYTHON/day02/myday01.py
@@ -0,0 +1,27 @@
+#minus, mul, div, mymod
+#멀티 리턴이 가능하다.
+def add(a, b):
+    return a+b
+def minus(a, b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def mymod(a, b):
+    return a%b
+
+sum = add(1,5)
+print(sum)
+sum = minus(2, 3)
+print(sum)
+sum = mul(2, 3)
+print(sum)
+sum = div(2, 3)
+print(sum)
+sum = mymod(2, 3)
+print(sum)
+
+
+
+
